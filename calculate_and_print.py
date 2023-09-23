cd5b003e488784484bf7c4958cf8a6801f3ca56f
Smizzyy <schmidt3798@yahoo.de>
2023-09-23 12:33:25 +0200

    changed age

--- calculate_and_print.py
+++ calculate_and_print.py
@@ -1,14 +1,14 @@
-myAge = 26
+myAge = 24
 
 # +, -, /, *
 
-#reche Seite wird immer zu erst ausgeführt und links abgespeichert
+# reche Seite wird immer zu erst ausgeführt und links abgespeichert
 myResult1 = myAge * 2
-int myResult2 = myAge / 2
+myResult2 = myAge / 2
 myResult3 = myAge + 2
 myResult4 = myAge - 2
 
-#Code in der Konsole ausgeben
+# Code in der Konsole ausgeben
 print(myResult1)
 print(myResult2)
 print(myResult3)
